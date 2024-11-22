2a909cc8a8246ec1fad751447567f5452acf528a
Carlomagno <carlolc@Carlomagnos-MacBook-Pro-2.local>
2024-11-21 22:26:22 -0600

    main: added icons in the list items

--- src/app/components/TaskListItem.tsx
+++ src/app/components/TaskListItem.tsx
@@ -20,8 +20,8 @@ const taskList = [
 export default function TaskListItem() {
     return (
         <ul role="list" className="divide-y divide-gray-100">
-            {taskList.map((task,index) => (
-                <li key={task.typeTask+index} className="flex justify-between gap-x-6 py-5">
+            {taskList.map((task, index) => (
+                <li key={task.typeTask + index} className="flex justify-between gap-x-6 py-5">
                     <div className="flex min-w-0 gap-x-4">
                         <div className="min-w-0 flex-auto">
                             <p className="text-left text-base/7  text-indigo-600">{task.typeTask}
@@ -44,7 +44,7 @@ export default function TaskListItem() {
                                 )}
                             </div>
                             <p className="text-gray-500 text-base/7">{task.taskName}</p>
-                          
+
                         </div>
                     </div>
                     <div className="ml-auto grid place-items-center justify-self-end">
@@ -52,6 +52,12 @@ export default function TaskListItem() {
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                                 <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                             </svg>
+                        </button>
+                        <button className="rounded-md border border-transparent p-2.5 text-center text-sm transition-all text-slate-600 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                            </svg>
+
                         </button>
                     </div>
 
